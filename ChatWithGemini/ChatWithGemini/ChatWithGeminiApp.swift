f62b0b56cf64752f016d31e9e6b3056e1b93ee74
KANISHK JOSHI <91533278+cyborg-joshi@users.noreply.github.com>
2024-05-14 09:45:07 +0530

    Add files via upload

---
+++ ChatWithGemini/ChatWithGemini/ChatWithGeminiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatWithGeminiApp.swift
+//  ChatWithGemini
+//
+//  Created by KANISHK on 13/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatWithGeminiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
